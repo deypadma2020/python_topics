97265ecd07be2142046a017c1dd7181c684caf21
deypadma2020 <deypadma2020@gmail.com>
2025-06-24 08:31:39 +0530

    What is Main Thread

---
+++ MultiThreading/test_03.py
@@ -0,0 +1,10 @@
+import threading
+print(threading.current_thread())
+print(threading.current_thread().name)
+print(threading.current_thread().ident)
+print(threading.current_thread().is_alive())
+
+
+
+
+
