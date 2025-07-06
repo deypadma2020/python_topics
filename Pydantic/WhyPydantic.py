fd5b4691ba86fe916f29651d7bfc341432a2bae5
deypadma2020 <deypadma2020@gmail.com>
2025-07-06 22:38:31 +0530

    update the insert func with another validation, so, Now need to update the update data func also

--- Pydantic/WhyPydantic.py
+++ Pydantic/WhyPydantic.py
@@ -1,8 +1,11 @@
 def insert_patient_data(name: str, age: int):
     if type(name) == str and type(age) == int:
-        print(name)
-        print(age)
-        print("Data inserted")
+        if age < 0:
+            raise ValueError("Age can't be negetive")
+        else:
+            print(name)
+            print(age)
+            print("Data inserted")
     else:
         raise TypeError("Incorrect DataType")
     
