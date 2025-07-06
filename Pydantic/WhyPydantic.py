8ede9248b152beaf48920a8a53bc5b7186be7628
deypadma2020 <deypadma2020@gmail.com>
2025-07-06 22:35:46 +0530

    working the func data validation, but for same data need to repeat same line of code

--- Pydantic/WhyPydantic.py
+++ Pydantic/WhyPydantic.py
@@ -5,6 +5,14 @@ def insert_patient_data(name: str, age: int):
         print("Data inserted")
     else:
         raise TypeError("Incorrect DataType")
+    
+def update_patient_data(name: str, age: int):
+    if type(name) == str and type(age) == int:
+        print(name)
+        print(age)
+        print("Data updated")
+    else:
+        raise TypeError("Incorrect DataType")
 
 insert_patient_data("Rajesh", 35)
 insert_patient_data("Rajesh", '35')
