647cba913c31ab9bad7f7846a282d0432e7fbd68
deypadma2020 <deypadma2020@gmail.com>
2025-07-08 12:46:31 +0530

    use of model_validator

---
+++ Pydantic/model_validator.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, EmailStr, model_validator
+from typing import List, Dict
+
+class Patient(BaseModel):
+    name: str
+    email: EmailStr
+    age: int
+    weight: float
+    married: bool
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+    @model_validator(mode='after')
+    def validator_emergency_contact(cls, model):
+
+        if model.age > 60 and 'emergency_contact' not in model.contact_details:
+            raise ValueError('Patients older than 60 must have an emergency contact')
+        
+        return model
+    
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print(patient.allergies)
+    print(patient.married)
+    print('updated')
+
+patient_info = {
+    'name':'Suman', 
+    'email':'abc@klizos.com', 
+    'age': '30', 
+    'weight': 75.2, 
+    'married': True, 
+    'allergies': ['pollen', 'dust'], 
+    'contact_details':{'phone':'257097', 'emergency_contact': '9434342578'}
+}
+
+patient1 = Patient(**patient_info) 
+
+update_patient_data(patient1)
